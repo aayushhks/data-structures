33aab60758de1536dacdf5236ed3aa49ed351603
aayush <159913583+aayushhks@users.noreply.github.com>
2024-08-03 19:36:17 +0530

    Add files via upload

---
+++ c/1.Array/linear.c
@@ -0,0 +1,51 @@
+// #include<stdio.h>
+
+// void linear(int arr[],int size, int element){
+//     for(int i=0; i<=size-1; i++){     //i<size both are correct
+//         if (arr[i]==element){
+//             printf("element found at index : %d\n",i);
+//             break;
+//         }
+//         else{
+//             printf("element not found\n");
+//             break;
+//         }
+//     }
+    
+
+// }
+// int main(){
+//     int arr[]={1,34,56,78,90};
+//     int element,size=sizeof(arr)/sizeof(int);
+//     printf("enter the element you want to search : ");
+//     scanf("%d",&element);
+//     linear(arr, 100, element);
+//     return 0;
+// }
+
+
+
+// sorted and unsorted for both we can use linear search but for sorted prefer binary
+
+// harry's code below
+
+#include<stdio.h>
+
+int linear(int arr[],int size, int element){
+    for(int i=0; i<=size-1; i++){     //i<size both are correct
+        if (arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={1,34,56,78,90};
+    int element,size=sizeof(arr)/sizeof(int);
+    printf("enter the element you want to search : ");
+    scanf("%d",&element);
+    int searchindex = linear(arr, 100, element);
+    printf("element %d was found at index %d \n", element, searchindex);
+    return 0;
+}
